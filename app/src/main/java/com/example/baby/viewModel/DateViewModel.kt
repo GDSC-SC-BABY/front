e5924c3621ded7720b4916c6f3d437d0509e7331
mi-fasol <minyoong39@naver.com>
2024-01-16 00:48:28 +0900

    feat: [dateViewModel] 오늘 날짜 반환하는 getDateNow() 함수 추가

--- app/src/main/java/com/example/baby/viewModel/DateViewModel.kt
+++ app/src/main/java/com/example/baby/viewModel/DateViewModel.kt
@@ -1,8 +1,5 @@
 package com.example.baby.viewModel
 
-import android.annotation.SuppressLint
-import android.os.Build
-import androidx.annotation.RequiresApi
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -11,20 +8,27 @@ import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.util.*
 
-
-@RequiresApi(Build.VERSION_CODES.O)
 class DateViewModel() : ViewModel() {
 
     private val _birthday = MutableLiveData<String>()
     val birthday: LiveData<String> = _birthday
 
     init {
-        // 초기 날짜를 오늘로 설정
         _birthday.value = LocalDate.now().format(DateTimeFormatter.ISO_LOCAL_DATE)
     }
 
-    // 날짜를 업데이트하는 메서드
     fun updateBirthday(newDate: LocalDate) {
         _birthday.value = newDate.format(DateTimeFormatter.ISO_LOCAL_DATE)
+    }
+
+    fun getDateNow(): String {
+        val now = System.currentTimeMillis()
+        val date = Date(now)
+        val dateFormat = SimpleDateFormat("yyyy년 MM월 dd일")
+
+//        val ddd = LocalDate.now()
+//        ddd.format(DateTimeFormatter.ofPattern("yyyy년 "))
+
+        return dateFormat.format(date)
     }
 }
