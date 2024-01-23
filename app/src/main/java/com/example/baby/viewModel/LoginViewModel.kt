3e499f49a8ff90bc7c8cd97498e1c4118978633d
seoyeon <seoyeonn051@gmail.com>
2024-01-23 23:58:10 +0900

    fix: loginViewModel 로그인 기능 생성

--- app/src/main/java/com/example/baby/viewModel/LoginViewModel.kt
+++ app/src/main/java/com/example/baby/viewModel/LoginViewModel.kt
@@ -1,21 +1,24 @@
 package com.example.baby.viewModel
 
 import android.content.Context
+import android.content.SharedPreferences
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.navigation.NavController
+import com.example.baby.network.AuthRepository
+import com.example.baby.network.FirebaseAuthRepository
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.firebase.auth.FirebaseAuth
-//import com.google.firebase.auth.FirebaseAuth
-import kotlinx.coroutines.async
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.asSharedFlow
-import kotlinx.coroutines.launch
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
-class LoginViewModel : ViewModel() {
-    private val _navigateToMainScreen = MutableLiveData<Boolean>()
+class LoginViewModel(private val repository: FirebaseAuthRepository) : ViewModel() {
+/*    private val _navigateToMainScreen = MutableLiveData<Boolean>()
     val navigateToMainScreen: LiveData<Boolean> = _navigateToMainScreen
 
     private lateinit var googleSignInClient: GoogleSignInClient
@@ -42,5 +45,58 @@ class LoginViewModel : ViewModel() {
         viewModelScope.launch {
             _loginResult.emit(isLogin)
         }
+    }*/
+var pref: SharedPreferences? = null
+    private var _isChecked = MutableLiveData<Boolean>()
+    val isChecked: LiveData<Boolean>
+        get() = _isChecked
+
+    private var authRepository: FirebaseAuthRepository = repository
+    //private var fireStoreRepo: FireStoreRepository = fireStoreRepository
+    private val _userLiveData = authRepository.userLiveData
+
+    val userLiveData: LiveData<FirebaseUser>
+        get() = _userLiveData
+
+    // set 함수는 이렇게 만드는 게 맞나? 아니면 더 간단한 방법이 있을까?
+    fun setCheckedValue(value: Boolean) {
+        _isChecked.value = value
+        isAutoLogin()
+    }
+
+    private fun getAuthRepository(authRepo: FirebaseAuthRepository){
+    }
+
+    suspend fun signUp(navController: NavController, email: String, pw: String){
+        withContext(Dispatchers.Main) {
+            val result = authRepository.signUp(email, pw)
+            //fireStoreRepo.createUser(result)
+            //유저 값 DB에 저장
+        }
+    }
+    fun signInUser(email: String, pw: String){
+        authRepository.signIn(email, pw)
+    }
+
+    private fun isAutoLogin(){
+        if(isChecked.value == true){
+            pref?.edit()?.apply {
+                putBoolean("AutoLoginChecked", true)
+                apply()
+                Log.d("SharedPreference",
+                    pref?.getBoolean("AutoLoginChecked", true).toString()
+                )
+            }
+        }
+        else{
+            pref?.edit()?.apply {
+                putBoolean("AutoLoginChecked", false)
+                apply()
+                Log.d("SharedPreference",
+                    pref?.getBoolean("AutoLoginChecked", false).toString()
+                )
+            }
+        }
+
     }
 }
