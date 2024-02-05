b0c26b7c52051ad90d864eaabed91a2ffd1e9a8b
mi-fasol <minyoong39@naver.com>
2024-02-05 17:27:31 +0900

    feat: 마이페이지 아기 정보 추가

---
+++ app/src/main/java/com/example/baby/data/Baby.kt
@@ -0,0 +1,9 @@
+package com.example.baby.data
+
+data class Baby(
+    val name: String,
+    val birth: String,
+    val gender: String,
+    val weight: String,
+    val height: String
+)
--- app/src/main/java/com/example/baby/screen/MyPageScreen.kt
+++ app/src/main/java/com/example/baby/screen/MyPageScreen.kt
@@ -1,22 +1,28 @@
 package com.example.baby.screen
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.DateRange
-import androidx.compose.material.icons.filled.List
-import androidx.compose.material.icons.filled.Person
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.baby.R
 import com.example.baby.util.CustomBottomNavigation
+import com.example.baby.util.SharedPreferenceUtil
 import com.example.baby.viewModel.LoadingViewModel
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
@@ -25,15 +31,72 @@ fun MyPageScreen(viewModel: LoadingViewModel, navController: NavController) {
     val navigateToMainScreen by viewModel.navigateToMainScreen.observeAsState()
     Scaffold(
         bottomBar = { CustomBottomNavigation(navController = navController) }
-    ) {
-        Box(
-            modifier = Modifier
-                .background(Color.White)
-                .fillMaxSize(),
-            contentAlignment = Alignment.Center
-
-        ) {
-            Text("MyPage")
+    ) { innerPadding ->
+        BoxWithConstraints {
+            Column(
+                modifier = Modifier
+                    .background(Color.White)
+                    .fillMaxSize()
+                    .padding(
+                        start = 20.dp,
+                        end = 20.dp,
+                        top = 20.dp,
+                        bottom = innerPadding.calculateBottomPadding() + 20.dp
+                    )
+                    .verticalScroll(rememberScrollState())
+            ) {
+                babyInfoCard()
+            }
         }
+    }
+}
+
+@Composable
+fun babyInfoCard() {
+    val context = LocalContext.current
+
+    Card(
+        shape = RoundedCornerShape(20.dp),
+        backgroundColor = Color.LightGray,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Row (modifier = Modifier.padding(10.dp)){
+            Image(
+                painter = painterResource(id = R.drawable.teddy_bear),
+                contentDescription = "babyPhoto",
+                modifier = Modifier.size(100.dp)
+            )
+            Spacer(modifier = Modifier.width(10.dp))
+            Column(
+                verticalArrangement = Arrangement.Center
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        SharedPreferenceUtil(context).getString("babyName", "").toString(),
+                        fontWeight = FontWeight.SemiBold,
+                        fontSize = 25.sp
+                    )
+                    Spacer(modifier = Modifier.width(5.dp))
+                    Image(
+                        painter = painterResource(id = SharedPreferenceUtil(context).getInt("genderIcon", R.drawable.man_icon)),
+                        contentDescription = "gender",
+                        modifier = Modifier.size(23.dp)
+                    )
+                }
+                Text(SharedPreferenceUtil(context).getString("birth", "").toString())
+                Text("6개월")
+                Text("이유식 초기")
+            }
+
+        }
+    }
+}
+
+@Composable
+fun userInfo(){
+    Column(){
+
     }
 }
