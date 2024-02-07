7fe4988adb87785811955af00d2503c65ac45987
seoyeon <seoyeonn051@gmail.com>
2024-02-07 19:27:06 +0900

    feat: Userresponse 생성 및 유저 조회 API 추가

--- app/src/main/java/com/example/baby/data/User.kt
+++ app/src/main/java/com/example/baby/data/User.kt
@@ -1,7 +1,15 @@
 package com.example.baby.data
 
+import com.google.gson.annotations.SerializedName
+
 data class User(
     val nickname: String,
     val residence: String,
     val relation: String
 )
+
+data class UserResponse(
+    @SerializedName("name") val name: String,
+    @SerializedName("residence") val residence: String
+)
+
--- app/src/main/java/com/example/baby/network/retrofitAPI.kt
+++ app/src/main/java/com/example/baby/network/retrofitAPI.kt
@@ -1,11 +1,17 @@
 package com.example.baby.network
 
 import com.example.baby.data.User
+import com.example.baby.data.UserResponse
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface ApiService {
     @POST("user")
     suspend fun registerUser(@Body user: User): Response<User>
+
+    @GET("/user/{userId}")
+    suspend fun getUser(@Path("userId") userId: String): UserResponse
 }
